06849e222b6a51e8fa6b2bf0023a76800ff8cb84
Nguyen Hoang Gia Bao <giabao@evol.vn>
2022-08-10 18:57:42 +0700

    create test for data.javascript

---
+++ src/data.test.js
@@ -0,0 +1,18 @@
+import { it, expect, vi, describe } from 'vitest'
+import { generateReportData } from './data'
+
+describe('generateReportData()', () => {
+    it('should execute logFn function if provided', () => {
+        // Arrange
+        // this spy function is an empty function
+        const logger = vi.fn();
+
+        // Act
+        // pass the spy function to the testing function
+        generateReportData(logger)
+
+        // Assert
+        // Check whether the spy is called
+        expect(logger).toBeCalled();
+    })
+})
